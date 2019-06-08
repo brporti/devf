ebf9853cb2058bd419d7b23f603b54e5643a6a5c
Bryan <bryanbportillo@gmail.com>
2019-06-07 18:07:15 -0600

    Ejercicios de for(var i y la calculadora a medias

---
+++ Clase/datos-complejos/main.js
@@ -0,0 +1,46 @@
+// var objetoPerro = {
+//     //atributos: valor
+//     patas:4,
+//     osico: true,
+//     nombre:'Firulais',
+//     cola:'corta'
+// }
+
+// console.log(objetoPerro.patas);
+
+// //Arrays, arreglos, listas
+
+// var listaDeSaludos = ['Hola','Que ondi','Que p2']
+// var listaCalificaciones = [6,6,6,5,9]
+// var listaVariada = ['Perro',76,'Adios',false,true,33]
+
+// console.log(listaVariada[2]);
+// console.log(listaVariada.length);
+
+// //FOR
+
+// console.log('FOR');
+// for(var i = 0; i<99; i++){
+//     console.log(i);
+// }
+
+// 1.Hacer un programa que imprima en pantalla los números del 1 al 1000.
+// console.log('FOR');
+// for(var i = 1; i<1001; i++){
+//     console.log(i);
+// }
+
+// 2.Hacer un programa que imprima en pantalla los números del 1000 al 1.
+// console.log('FOR');
+// for(var i = 1000; i>0; i--){
+// console.log(i);
+// }
+
+
+// 3.Hacer un programa que imprima en pantalla 10,000 veces el siguiente mensaje:
+// "Este es el número: X" remplazando X con los numeros del 1 al 10,000.
+// for(var i = 0; i<10001; i++){
+// console.log('Este es el numero' + i);
+// }
+
+ 

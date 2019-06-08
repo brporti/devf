6375f397a78d4815a48a0ba91f00ef458c9de4af
Bryan <bryanbportillo@gmail.com>
2019-06-07 20:49:56 -0600

    ejercicios de JavaScript intermedio y lo de los contactos

---
+++ Clase/js-intermedio/main.js
@@ -0,0 +1,55 @@
+// var listaComida = ['Tacos','Chela','Pupusas','Tamales','Enchiladas', 'Pizza']
+
+// for (var i = 0; i < listaComida.length; i++){
+//     console.log('Esta es la iteracion numero'+ [i]);
+//     console.log(listaComida[i]);
+// }
+
+
+// 1.- Ingresa 100 valores en una lista y después imprímelo 
+// var lista = []
+// for ( var i = 0; i<100; i++){
+//     lista.push(i)
+// }
+// console.log(lista);
+
+// 2.- Pide un numero y guarda en una lista su tabla de multiplicar hasta el 10
+// var numero = 6
+// var listaMultiplo = []
+// for(var i = 1; i < 11; i++){
+//     listaMultiplo.push(i * numero)
+// }
+
+// console.log(listaMultiplo);
+
+// var  contacto = {
+//     nombre: 'Juanito', 
+//     edad: 30,
+//     trabajo: 'Cocinero',
+//     telefono: '77228899'
+// }
+
+// var  contacto2 = {
+//     nombre: 'Panchito', 
+//     edad: 41,
+//     trabajo: 'Cocinero',
+//     telefono: '77990044'
+// }
+
+// var  contacto3 = {
+//     nombre: 'Omar', 
+//     edad: 13,
+//     trabajo: 'Estudiante',
+//     telefono: '456789987'
+// }
+
+// var listaDeContactos = [contacto, contacto2, contacto3]
+
+// for(var i = 0; i< listaDeContactos.length; i++){
+//    if(listaDeContactos[i].edad > 18)
+//     console.log(listaDeContactos[i].nombre);
+
+// }
+
+
+    
